68d849ff27f4ac86fb498f96415c234ff4de741b
s7lima <bruno.eslima@senacsp.edu.br>
2024-09-27 20:12:53 -0300

    Feat: EX1 corrigido

---
+++ exercicios/ex1/js/script.js
@@ -0,0 +1,12 @@
+// Declaração Intero e decimal
+const text = "uma palavra";
+let numInteiro = 30;
+const numDecimal = 35.5;
+
+// Saída de dados 
+// forma 1
+console.log( text+", " + numInteiro + ", " + numDecimal)
+// forma 2
+console.log( text+', ' + numInteiro + ', ' + numDecimal)
+// forma 3
+console.log( `${text}, ${numInteiro}, ${numDecimal}`)
