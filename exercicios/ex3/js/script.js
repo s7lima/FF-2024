20094e4c53fd797dfa4efd169b39126e17394357
s7lima <bruno.eslima@senacsp.edu.br>
2024-09-27 21:13:44 -0300

    Feat: EX4  finalizado

---
+++ exercicios/ex3/js/script.js
@@ -0,0 +1,16 @@
+const constante1 = 1;
+const constante2 = "Texto";
+const constante3 = true;
+const constante4 = 4.75;
+const constante5 = "Texto 2";;
+
+let variavel1 = 1;
+let variavel2 = "texto 3";
+let variavel3 = true;
+let variavel4 = 4.49;
+let variavel5 = "Texto 4";
+
+let msg = `${constante1} ${constante2} ${constante3} ${constante4} ${constante5}`;
+msg += `${variavel1} ${variavel2} ${variavel3} ${variavel4} ${variavel5}`;
+
+console.log(msg);
